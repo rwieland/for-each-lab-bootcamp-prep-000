858ef01cf17ffb01bdd0dcab5a811fc60775f389
Robert Wieland <rwieland92@gmail.com>
2017-04-19 20:06:36 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,19 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var array = [1, 2, 3, 4]
+  array.forEach(element => {
+    callback(element)
+  })
+  return array
+}
+
+function doToArray(array, callback) {
+  array.forEach(element => {
+    callback(element)
+  })
+}
